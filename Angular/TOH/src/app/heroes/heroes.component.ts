f56b1ca8100d69143d01f5efe0e0c6ff0f133c5a
Gilvan2 <gilvan.silva.junior@gmail.com>
2024-01-09 23:39:09 -0300

    commit inicial para aprender typescript

---
+++ Angular/TOH/src/app/heroes/heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-heroes',
+  //standalone: true,
+  //imports: [CommonModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+
+  heroes = 'Blabla'
+
+}
